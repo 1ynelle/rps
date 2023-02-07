4b2518630deea0369557bdea44eab795225e4b13
Lynelle <l>
2023-02-06 16:13:21 -0800

    Modify monitor image display

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const domCpScore = document.querySelector('#cp-score');
 
 // Monitor
 const winnerText = document.querySelector('.cp-board__winner-text');
-const cpOptions = document.querySelectorAll('.cp-board__cp-option');
-const winnerImgs = document.querySelectorAll('.cp-board__winner-img')
+const cpChoiceImg = document.querySelector('.cp-board__cp-option');
+const winnerImg = document.querySelector('.cp-board__winner-img');
 const results = document.querySelector('.cp-board__results');
 const playAgainBtn = document.querySelector('.cp-board__play-again');
 
@@ -18,17 +18,13 @@ const plOptions = document.querySelectorAll('.pl-board__pl-option');
 
 // Starting Screen
 function startGame() {
-    playerScore = 0;
-    domPlScore.innerText = playerScore;
-    computerScore = 0;
-    domCpScore.innerText = computerScore;
+    resetScores();
 
     winnerText.classList.add('hidden');
-    winnerImgs.forEach(img => {
-        img.classList.add('hidden');
-    });
+    winnerImg.classList.add('hidden');
     playAgainBtn.classList.add('hidden');
 
+    cpChoiceImg.classList.remove('hidden');
     results.classList.remove('hidden');
     plBoard.style.display = 'flex';
 
@@ -40,8 +36,8 @@ function startGame() {
 startGame();
 
 // Gameflow for each round
-function playRound(e) {
-    const playerChoice = e.currentTarget.getAttribute('data-option');
+function playRound() {
+    const playerChoice = this.getAttribute('data-option');
     const computerChoice = getComputerChoice();
 
     showPlayerChoice(playerChoice);
@@ -54,9 +50,16 @@ function playRound(e) {
 }
 
 // Helper Functions
+function resetScores() {
+    playerScore = 0;
+    computerScore = 0;
+    domPlScore.innerText = playerScore;
+    domCpScore.innerText = computerScore;
+}
+
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
-    const index = Math.floor(Math.random() * 3);
+    const index = Math.floor(Math.random() * options.length);
     return options[index];
 }
 
@@ -72,13 +75,8 @@ function showPlayerChoice(playerChoice) {
 }
 
 function showComputerChoice(computerChoice) {
-    cpOptions.forEach(option => {
-        if (option.getAttribute('data-option') === computerChoice) {
-            option.classList.remove('hidden');
-        } else {
-            option.classList.add('hidden');
-        }
-    });
+    cpChoiceImg.src = `img/cp-${computerChoice}.png`;
+    cpChoiceImg.alt = `${computerChoice}`;
 }
 
 function determineWinner(playerChoice, computerChoice) {
@@ -87,7 +85,7 @@ function determineWinner(playerChoice, computerChoice) {
 
     } else if (playerChoice === "rock" && computerChoice === "scissors" ||
                playerChoice === "paper" && computerChoice === "rock" ||
-               playerChoice === "scissors") {
+               playerChoice === "scissors" && computerChoice === "paper") {
         domPlScore.innerText = ++playerScore;
         results.innerText = "Player wins! 😊";
 
@@ -98,11 +96,8 @@ function determineWinner(playerChoice, computerChoice) {
 }
 
 function checkScore() {
-    if (playerScore === 5) {
-        showFinalWinner("player");
-    } else if (computerScore === 5) {
-        showFinalWinner("computer");
-    }
+    if (playerScore === 5) showFinalWinner("player");
+    if (computerScore === 5) showFinalWinner("computer");
 }
 
 function resetGameboard() {
@@ -114,33 +109,24 @@ function resetGameboard() {
             option.addEventListener('click', playRound);
         });
 
-        cpOptions.forEach(option => {
-            option.classList.add('hidden');
-        });
+        cpChoiceImg.removeAttribute('src');
+        cpChoiceImg.removeAttribute('alt');
     }, 1300);
 }
 
 function showFinalWinner(winner) {
+    cpChoiceImg.classList.add('hidden');
+    results.classList.add('hidden');
+    plBoard.style.display = 'none';
+
     winnerText.classList.remove('hidden');
 
-    cpOptions.forEach(option => {
-        option.classList.add('hidden');
-    });
-
-    const playerImage = document.querySelector('#pl-winner-img');
-    const computerImage = document.querySelector('#cp-winner-img');
-    if (winner === "player") {
-        playerImage.classList.remove('hidden');
-    } else if (winner === "computer") {
-        computerImage.classList.remove('hidden');
-    }
-
-    results.classList.add('hidden');
+    winnerImg.classList.remove('hidden');
+    winnerImg.src = `img/${winner}.png`;
+    winnerImg.alt = `${winner}`;
 
     playAgainBtn.classList.remove('hidden');
     playAgainBtn.addEventListener('click', e => {
         startGame();
     });
-   
-    plBoard.style.display = 'none';
 }
